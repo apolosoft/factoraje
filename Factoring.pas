c6d19fa8bed62178497471ca66ae5b5b83457879
apolosoft <proyectos@apolosoft.com>
2014-08-11 15:53:28 -0500

    VERSION 5.2.0 DE MEKANO

--- Factoring.pas
+++ Factoring.pas
@@ -4,29 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs,uBase,IniFiles,dBClient,
-  cxGraphics, cxControls, cxLookAndFeels,
-  cxLookAndFeelPainters, cxStyles, dxSkinsCore, dxSkinBlack, dxSkinBlue,
-  dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkRoom, dxSkinDarkSide,
-  dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
-  dxSkinGlassOceans, dxSkinHighContrast, dxSkiniMaginary, dxSkinLilian,
-  dxSkinLiquidSky, dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMetropolis,
-  dxSkinMetropolisDark, dxSkinMoneyTwins, dxSkinOffice2007Black,
-  dxSkinOffice2007Blue, dxSkinOffice2007Green, dxSkinOffice2007Pink,
-  dxSkinOffice2007Silver, dxSkinOffice2010Black, dxSkinOffice2010Blue,
-  dxSkinOffice2010Silver, dxSkinOffice2013DarkGray, dxSkinOffice2013LightGray,
-  dxSkinOffice2013White, dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic,
-  dxSkinSharp, dxSkinSharpPlus, dxSkinSilver, dxSkinSpringTime, dxSkinStardust,
-  dxSkinSummer2008, dxSkinTheAsphaltWorld, dxSkinsDefaultPainters,
-  dxSkinValentine, dxSkinVS2010, dxSkinWhiteprint, dxSkinXmas2008Blue,
-  dxSkinscxPCPainter, cxCustomData, cxFilter, cxData, cxDataStorage, cxEdit,
-  cxNavigator, DB, cxDBData, dxSkinsdxBarPainter, dxLayoutContainer,
-  cxGridViewLayoutContainer, cxGridLayoutView, cxGridDBLayoutView,
-  cxGridCustomLayoutView, dxBar, dxBarExtDBItems, cxClasses, cxGridLevel,
-  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGridCustomView,
-  cxGrid, cxContainer, IdBaseComponent, IdComponent, IdTCPConnection,
-  IdTCPClient, IdHTTP, cxTextEdit, cxMemo, cxGridBandedTableView,
-  cxGridDBBandedTableView;
+  Dialogs,uBase,IniFiles,dBClient;
+
 type
   TFFactoring = class(TFBase)
     Grid: TcxGrid;
@@ -636,6 +615,8 @@ end;
 procedure TFFactoring.Imprime(sReporte, sNumero : String);
 Begin
 
+// SE PASA LA VARIABLE NUMERO PARA PODER ENLAZAR EL REPORTE
+
   if not FileExists('REPORTES/'+sReporte) then
      raise Exception.Create('El archivo vinculado no existe');
 
@@ -693,6 +674,8 @@ end;
 procedure TFFactoring.PreImprime(sReporte, sNumero : String);
 Begin
 
+
+// SE PASA LA VARIABLE NUMERO PARA PODER ENLAZAR EL REPORTE
 
   if not FileExists('REPORTES/'+sReporte) then
      raise Exception.Create('El archivo vinculado no existe');
